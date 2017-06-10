5b04fa21d6bd6ad1295c35ef6057ef409ec131e3
Vlad Bilanchuk <burning1848@gmail.com>
2017-06-10 17:15:43 +0300

    123

--- homework.rb
+++ homework.rb
@@ -9,6 +9,7 @@ def generate
   first_part = ->()  { (LETTERS_RANGE).to_a.sample(FIRST_PART_LENGTH).join }
   second_part = ->() { (DIGITS_RANGE).to_a.sample(SECOND_PART_LENGTH).join }
   third_part = ->() { (LETTERS_RANGE).to_a.sample(THIRD_PART_LENGTH).join }
-  result = "#{first_part.call}-#{second_part.call}-#{third_part.call}
+  result = "#{first_part.call}-#{second_part.call}-#{third_part.call}"
 end
 
+123
